9ab7b16b8b3b83bfdea505444998c1dc102c278a
raul <raul425borges@gmail.com>
2024-03-20 09:47:42 -0300

    novas janelas

--- interface.py
+++ interface.py
@@ -9,13 +9,16 @@ co2 = "#3fb5a3"  # verde / green
 co3 = "#38576b"  # valor / value
 co4 = "#403d3d"   # letra / letters
 
-file_name = 'usuarios.dat'
+senhas = 'senhas.dat'
 
 def janela1():
     def verificar():
         senha = senha_entry.get()
+        with open(senhas, 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([1234])
         # Verificar se o nome de usuário e a senha correspondem aos armazenados no arquivo "users.dat"
-        with open('usuarios.dat', 'r', newline = '') as file:
+        with open(senhas, 'r', newline = '') as file:
             reader = csv.reader(file)
             for linha in reader:
                 if linha[0] == senha:
@@ -31,24 +34,24 @@ def janela1():
     janela.minsize(width=260, height=270)
     janela.title("Login")
 
-    frame_cima = Frame(janela, width=310, height=70, bg=co1, relief='flat')
+    frame_cima = Frame(janela, width=310, height=70, bg=co0, relief='flat')
     frame_cima.grid(row=0, column=0, padx=1, pady=0, sticky=NSEW)
-    frame_baixo = Frame(janela, width=310, height=230, bg=co1, relief='flat')
+    frame_baixo = Frame(janela, width=310, height=230, bg=co0, relief='flat')
     frame_baixo.grid(row=1, column=0, padx=1, pady=0, sticky=NSEW)
 
     # Criar os widgets
-    login = tk.Label(frame_cima, anchor=NE, text="LOGIN", font="Ivy 25", bg=co1)
+    login = tk.Label(frame_cima, anchor=NE, text="LOGIN", font="Ivy 25", bg=co0)
     login.place(x=5, y=5)
 
     linha = tk.Label(frame_cima, width=275, text="", font="Ivy 1", bg="#660101")
     linha.place(x=-2, y=45)
 
-    senha = tk.Label(frame_baixo, text="senha:", bg=co1)
+    senha = tk.Label(frame_baixo, text="senha:", bg=co0)
     senha.place(x=10, y=30)
     senha_entry = tk.Entry(frame_baixo, show="*", relief="flat", width=25)
     senha_entry.place(x=10, y=60)
 
-    login = tk.Button(frame_baixo, text="enter", command=verificar, width=25, height=2, bg="#660101", relief="flat")
+    login = tk.Button(frame_baixo, text="enter",fg=co1, command=verificar, width=25, height=2, bg="#660101", relief="flat")
     login.place(x=10, y=120)
 
     janela.mainloop()
@@ -64,7 +67,10 @@ def janela3():
         
     janela = tk.Tk()
     janela.geometry('350x350')
-    janela.title("INFO")
+    janela.title("")
+    janela.maxsize(width=350, height=350)
+    janela.minsize(width=350, height=350)
+
     
     frame_cima = Frame(janela, width=350, height=60, bg=co0, relief='flat')
     frame_cima.grid(row=0, column=0, padx=1, pady=0, sticky=NSEW)
@@ -97,10 +103,10 @@ def janela3():
     telefone = tk.Entry(frame_medio, relief="flat", width=25)
     telefone.grid(row=7, column=1, sticky=NSEW)
 
-    sair = tk.Button(frame_baixo, text="Voltar", command = voltar, width=13, height=2, bg="#951C1C", relief="flat")
+    sair = tk.Button(frame_baixo, text="Voltar",fg=co1, command = voltar, width=13, height=2, bg="#951C1C", relief="flat")
     sair.place(x=20, y=60)
     
-    proximo = tk.Button(frame_baixo, text="Próximo", command = avançar, width=13, height=2, bg="#951C1C", relief="flat")
+    proximo = tk.Button(frame_baixo, text="Próximo",fg=co1, command = avançar, width=13, height=2, bg="#951C1C", relief="flat")
     proximo.place(x=220, y=60)
     
     janela.mainloop()
@@ -113,20 +119,86 @@ def janela2():
         janela3()
     janela = tk.Tk()
     janela.geometry('350x350')
-    janela.title("INFO")
+    janela.title("HOME")
+    janela.winfo_rgb(co0)
 
-    ccliente = tk.Button( text="Criar Pedido", width=33, command=criarPedido, height=3, bg="orange", relief="flat")
-    ccliente.place(x=50, y=10)
+    ccliente = tk.Button(text="Criar Pedido", width=33,fg=co1, command=criarPedido, height=3, bg="orange", relief="flat")
+    ccliente.place(x=50, y=50)
 
-    cpedido = tk.Button( text="Ver Pedidos", width=33, height=3, bg="orange", relief="flat")
-    cpedido.place(x=50, y=100)
+    cpedido = tk.Button(text="Ver Pedidos", width=33,fg=co1, height=3, bg="orange", relief="flat")
+    cpedido.place(x=50, y=140)
 
-    sair = tk.Button( text="Sair",command = sair, width=20, height=2, bg="red", relief="flat")
-    sair.place(x=100, y=200)
+    sair = tk.Button(text="Sair",command = sair,fg=co1, width=20, height=2, bg="red", relief="flat")
+    sair.place(x=100, y=240)
 
 def janela4():
+    def voltar():
+        janela.destroy()
+        janela3()       
+    
+    def avançar():
+        janela.destroy()
+        janela5()
+        
     janela = tk.Tk()
     janela.geometry('350x350')
-    janela.title("INFO")
-print("hello")
-janela3()
+    janela.title("")
+    janela.maxsize(width=350, height=350)
+    janela.minsize(width=350, height=350)
+
+    
+    frame_cima = Frame(janela, width=350, height=60, bg=co0, relief='flat')
+    frame_cima.grid(row=0, column=0, padx=1, pady=0, sticky=NSEW)
+    frame_medio = Frame(janela, width=350, height=250, bg=co0, relief='flat')
+    frame_medio.grid(row=1, column=0, padx=1, pady=0, sticky=NSEW)
+    frame_baixo = Frame(janela, width=350, height=200, bg=co0, relief='flat')
+    frame_baixo.grid(row=2, column=0, padx=1, pady=0, sticky=NSEW)
+    
+    titulo = tk.Label(frame_cima, text="Cadastrar Pedido", font="Arial 18", bg=co0)
+    titulo.place(x=70, y=10)
+       
+    marca = tk.Label(frame_medio, text="marca:", bg=co0)
+    marca.grid(row=0, column=1, sticky=NSEW)
+    marca = tk.Entry(frame_medio, relief="flat", width=25)
+    marca.grid(row=1, column=1, sticky=NSEW)
+    
+    
+    ano = tk.Label(frame_medio, text="ano:", bg=co0)
+    ano.grid(row=2, column=1, sticky=NSEW)
+    ano = tk.Entry(frame_medio, relief="flat", width=25)
+    ano.grid(row=3, column=1, sticky=NSEW)
+    
+    placa = tk.Label(frame_medio, text="placa:", bg=co0)
+    placa.grid(row=4, column=1, sticky=NSEW)
+    placa = tk.Entry(frame_medio, relief="flat", width=25)
+    placa.grid(row=5, column=1, sticky=NSEW)
+    
+    descricao = tk.Label(frame_medio, text="descrição do pedido:", bg=co0)
+    descricao.place(x=180, y=0)
+    descricao = tk.Entry(frame_medio, relief="flat", width=25)
+    descricao.place(x=180, y=20)
+
+    sair = tk.Button(frame_baixo, text="Voltar", command = voltar, width=13, height=2, fg=co1, bg="#951C1C", relief="flat")
+    sair.place(x=20, y=60)
+    
+    proximo = tk.Button(frame_baixo, text="Próximo", command = avançar, width=13, height=2, fg=co1, bg="#951C1C", relief="flat")
+    proximo.place(x=220, y=60)
+    
+    janela.mainloop()
+    
+def janela5():
+    def voltar():
+        janela.destroy()
+        janela3()       
+    
+    def avançar():
+        janela.destroy()
+        janela2()
+        
+    janela = tk.Tk()
+    janela.geometry('350x350')
+    janela.title("")
+    janela.maxsize(width=350, height=350)
+    janela.minsize(width=350, height=350)
+    
+janela1()
